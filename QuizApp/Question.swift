5a704eb36becf9eb0ed62e419a79fb0542e2f826
ac1ra <ac1ra@iMac.local>
2022-07-15 13:58:49 +0300

    add files

---
+++ QuizApp/Question.swift
@@ -0,0 +1,29 @@
+//
+//  Model.swift
+//  QuizApp
+//
+//  Created by ac1ra on 14.07.2022.
+//
+
+import Foundation
+class Question{
+    let questionImage:String
+    let question:String
+    let optionA:String
+    let optionB:String
+    let optionC:String
+    let optionD:String
+    
+    let correctAnswer:Int
+    
+    init(image: String, questionText: String, choiceA: String, choiceB: String, choiceC: String, choiceD: String, answer: Int) {
+        
+        questionImage = image
+        question = questionText
+        optionA = choiceA
+        optionB = choiceB
+        optionC = choiceC
+        optionD = choiceD
+        correctAnswer = answer
+    }
+}
--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -8,7 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var progressView: UIView!
+    @IBOutlet weak var questionCounter: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var flagView: UIImageView!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    //outlet for Buttons
+    @IBOutlet weak var optionA: UIButton!
+    @IBOutlet weak var optionB: UIButton!
+    @IBOutlet weak var optionC: UIButton!
+    @IBOutlet weak var optionD: UIButton!
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if sender.tag == 1{
+            print("option a")
+        }else if sender.tag == 2{
+            print("option b")
+        }else if sender.tag == 3{
+            print("option c")
+        }else if sender.tag == 4{
+            print("option d")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
